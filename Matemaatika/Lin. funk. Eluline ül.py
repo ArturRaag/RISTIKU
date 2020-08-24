ba2ba3bda70942bf52abc9cb390d0245dd46817a
Artur Raag <68288831+ArturRaag@users.noreply.github.com>
2020-08-24 15:31:12 +0300

    Update Lin. funk. Eluline ül.py

--- Matemaatika/Lin. funk. Eluline ül.py
+++ Matemaatika/Lin. funk. Eluline ül.py
@@ -9,8 +9,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 palk=570
 kulu_päevas=10
 üür=160
@@ -19,13 +17,11 @@ x=np.arange(0,31)
 
 jaak=palk-üür-kulu_päevas*x
 
-
 plt.plot(x,jaak)
 plt.ylabel("y-telg")
 plt.xlabel("x-telg")
 plt.plot(0,0,marker="o",color="red")
 plt.annotate("0-punkt", (0,0))
-
 
 plt.grid(True)
 plt.show()
