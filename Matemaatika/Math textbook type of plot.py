9d5b143b02e7d3cd1b8782b1b59dac1d52f69aad
Artur Raag <68288831+ArturRaag@users.noreply.github.com>
2021-01-24 20:21:10 +0200

    Update Math textbook type of plot.py

--- Matemaatika/Math textbook type of plot.py
+++ Matemaatika/Math textbook type of plot.py
@@ -4,12 +4,10 @@ Created on Sun Aug 23 07:55:34 2020
 
 @author: artur
 """
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.style.use("classic")
+plt.style.use("default")
 
 fig, ax = plt.subplots()
 # Move the left and bottom spines to x = 0 and y = 0, respectively.
@@ -18,14 +16,27 @@ ax.spines["bottom"].set_position(("data",0))
 # Hide the top and right spines
 ax.spines["top"].set_visible(False)
 ax.spines["right"].set_visible(False)
+ax.plot(1,0,">k",transform=ax.get_yaxis_transform(),clip_on=False)
+ax.plot(0,1,"^k",transform=ax.get_xaxis_transform(),clip_on=False) # noolte juures saad värvi muuta, nt ">k" annaks musta värvi noole
 
-ax.plot(1,0,">",transform=ax.get_yaxis_transform(),clip_on=False)
-ax.plot(0,1,"^",transform=ax.get_xaxis_transform(),clip_on=False) # noolte juures saad värvi muuta, nt ">k" annaks musta värvi noole
 
-x=np.arange(-100.0,100.0,0.01)
-y=x**2+3*x-15
+# Siin saab jaotiste väärtusi muuta omal soovil. np.arange(algus, lõpp, samm)
+ax.set_xticks(np.arange(-10, 10 , 1))
+ax.set_yticks(np.arange(-10 ,10 , 1))
+ax.set_ylim(-6,6)
+ax.set_xlim(-6,6)
+ax.grid(True)
+ax.set_title("y=ax+b graafik", fontsize=18)
 
-ax.plot(x,y)
-plt.grid(True)
-plt.plot(0,0,"o",color="red")
-plt.show()
+# FUNKTSIOONE/PUNKTE JA MUID ASJU SAAD MUUTA SIIN.
+
+def linfunk(x):
+    return -3*x+2 # See on meie funktsioon y=ax+b
+
+x=np.linspace(-10,10,50) # Määramispiirkond 
+y=linfunk(x) 
+ax.plot(x,y, lw=2) # Joonestame joone
+
+x_punktid=[-1, 1] # See on justkui meie tabelis x-ide rida.
+for i in x_punktid:
+    ax.plot(i, linfunk(i), "ro") # Märgime graafikule punktid
